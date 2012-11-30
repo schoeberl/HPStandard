1463540c5bbba06294586864e72f0918a753e413
Martin Schoeberl <martin@jopdesign.com>
2012-11-30 00:05:37 -0800

    test: back to the original YARI source for simulation

--- scripts/yarisim.sh
+++ scripts/yarisim.sh
@@ -2,8 +2,8 @@
 
 mkdir build-yarisim
 cd build-yarisim
-# git clone git://github.com/tommythorn/yari.git
-git clone git://github.com/schoeberl/yari.git
+git clone git://github.com/tommythorn/yari.git
+# git clone git://github.com/schoeberl/yari.git
 cd yari/shared/yarisim
 make yarisim
 cp yarisim ../../../../bin
